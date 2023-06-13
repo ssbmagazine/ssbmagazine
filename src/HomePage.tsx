c552f79fe47f9b6d7df4bb51f8bf9a81173b44d6
Sai Sudhir <saisudhirs@outlook.com>
2023-06-13 13:34:42 +0530

    init commit

---
+++ src/HomePage.tsx
@@ -0,0 +1,10 @@
+import {Paper, Typography} from "@mui/material";
+
+export default function HomePage() {
+    return <Paper>
+        <Typography>Aum Sri Sai Ram</Typography>
+        <Typography >Sathya Sai Balavikas - Telugu Monthly Magazine</Typography>
+        <img src={"/logo.png"}/>
+
+    </Paper>
+}
